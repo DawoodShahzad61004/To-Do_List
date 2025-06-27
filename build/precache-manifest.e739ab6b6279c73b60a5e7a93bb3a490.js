681855dc8f3dc0f5eeb3ac3cfe5e16eae7f51054
Dawood Shahzad <139591208+DawoodShahzad61004@users.noreply.github.com>
2025-06-27 08:36:28 +0500

    Add files via upload

---
+++ build/precache-manifest.e739ab6b6279c73b60a5e7a93bb3a490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf1da16b73c759a14bfbfa10019c497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3438b915c602f47ea60",
+    "url": "/static/css/main.2fe6779e.chunk.css"
+  },
+  {
+    "revision": "21f381f2bc137d45bf5d",
+    "url": "/static/js/2.0555e64f.chunk.js"
+  },
+  {
+    "revision": "c3438b915c602f47ea60",
+    "url": "/static/js/main.c00bd90a.chunk.js"
+  },
+  {
+    "revision": "2b39c98d96c9238cdd2e",
+    "url": "/static/js/runtime-main.28f67ebe.js"
+  }
+]);
