a94e162f268d78d333bb538cc1f77d045827f30e
DawoodShahzad61004 <dawoodbutt590@gmail.com>
2025-07-10 16:05:56 +0500

    Updates

---
+++ precache-manifest.e587e834b93315a89dbbf12bd0757443.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685df1e04d7b377dabffcd8e34aad0ae",
+    "url": "/To-Do_List/index.html"
+  },
+  {
+    "revision": "21f381f2bc137d45bf5d",
+    "url": "/To-Do_List/static/js/2.0555e64f.chunk.js"
+  },
+  {
+    "revision": "5a00a96c3d9f76a1b7d4",
+    "url": "/To-Do_List/static/js/main.617bb5bb.chunk.js"
+  },
+  {
+    "revision": "3fefdfaa30264208aee6",
+    "url": "/To-Do_List/static/js/runtime-main.abae7bfb.js"
+  }
+]);
